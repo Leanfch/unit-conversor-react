4a1c4a0c73cbec421371d5aeace571eeeaabdc6e
Leandro Fernandez <leanf94@gmail.com>
2025-08-20 21:49:45 -0300

    get the values of the object "selected"

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import { useState, type ChangeEvent } from "react";
 
 const data = [
-    {
-        id: "celsius",
-        name: "Celsius",
-        unit: "° C"
-    },
-    {
-        id: "fahrenheit",
-        name: "Fahrenheit",
-        unit: "° F"
-    },
-    {
-        id: "kelvin",
-        name: "Kelvin",
-        unit: "K"
-    },
-]
+	{
+		id: "celsius",
+		name: "Celsius",
+		unit: "° C",
+	},
+	{
+		id: "fahrenheit",
+		name: "Fahrenheit",
+		unit: "° F",
+	},
+	{
+		id: "kelvin",
+		name: "Kelvin",
+		unit: "K",
+	},
+];
 
 function App() {
-    const [unit1, setUnit1 ] = useState(data[0].name);
-    const [unit2, setUnit2 ] = useState(data[1].name);
+	const [unit1, setUnit1] = useState(data[0]);
+	const [unit2, setUnit2] = useState(data[1]);
 
-    function handleChange(event: ChangeEvent) {
-        setUnit1(event.target.value);
-    }
+	function handleChange(event: ChangeEvent) {
+		setUnit1(data.find((item) => item.name === event.target.value));
+	}
+
+	function handleChange2(event: ChangeEvent) {
+		setUnit2(data.find((item) => item.name === event.target.value));
+	}
 
 	return (
 		<section className="bg-teal-950 w-screen h-screen flex flex-col items-center justify-center">
@@ -38,36 +42,48 @@ function App() {
 							name="temp"
 							id="temp"
 							className="bg-gray-900 p-1 rounded-md pr-5"
-                            value={unit1}
-                            onChange={handleChange}
+							value={unit1.name}
+							onChange={handleChange}
 						>
-                            {data.map( (unit) => {
-                                return (<option value={unit.name} key={unit.id}>{unit.name}</option>)
-                            })}
+							{data.map((unit) => {
+								return (
+									<option value={unit.name} key={unit.id}>
+										{unit.name}
+									</option>
+								);
+							})}
 						</select>
 						<input
 							type="number"
 							placeholder="0"
 							className="bg-gray-100 text-gray-800 placeholder:text-gray-600 p-2 rounded-md mx-5"
 						/>
-                        <span>° C </span>{ /* DEBE SER DINAMICO */}
+						<span>{unit1.unit}</span>
+						{/* DEBE SER DINAMICO */}
 					</div>
 					<div>
 						<select
 							name="temp"
 							id="temp"
 							className="bg-gray-900 p-1 rounded-md pr-5"
-                            >
-                            {data.map( (unit) => {
-                                return (<option value={unit.name} key={unit.id}>{unit.name}</option>)
-                            })}
+							value={unit2.name}
+							onChange={handleChange2}
+						>
+							{data.map((unit) => {
+								return (
+									<option value={unit.name} key={unit.id}>
+										{unit.name}
+									</option>
+								);
+							})}
 						</select>
 						<input
 							type="number"
 							placeholder="0"
 							className="bg-gray-100 text-gray-800 placeholder:text-gray-600 p-2 rounded-md mx-5"
-                            />
-                        <span>° C </span>{ /* DEBE SER DINAMICO */}
+						/>
+						<span>{unit2.unit}</span>
+						{/* DEBE SER DINAMICO */}
 					</div>
 				</section>
 			</article>
