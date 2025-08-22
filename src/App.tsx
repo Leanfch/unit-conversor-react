4055968adea7a492426f1b9a9baf1536ca581e6d
Leandro Fernandez <leanf94@gmail.com>
2025-08-22 18:55:35 -0300

    fix: ensure default unit selection in handleChange functions

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,12 @@ function App() {
 	const [unit1, setUnit1] = useState(data[0]);
 	const [unit2, setUnit2] = useState(data[1]);
 
-	function handleChange(event: ChangeEvent) {
-		setUnit1(data.find((item) => item.name === event.target.value));
+	function handleChange(event: ChangeEvent<HTMLSelectElement>) {
+		setUnit1(data.find((item) => item.name === event.target.value) ?? data[0]);
 	}
 
-	function handleChange2(event: ChangeEvent) {
-		setUnit2(data.find((item) => item.name === event.target.value));
+	function handleChange2(event: ChangeEvent<HTMLSelectElement>) {
+		setUnit2(data.find((item) => item.name === event.target.value) ?? data[0]);
 	}
 
 	return (
